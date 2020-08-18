5998490e358f9f5f7685ac9b7174462923d5919d
Guilherme Henrique Miserski <guilhermemiserski@hotmail.com>
2020-08-18 14:58:24 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,18 +9,20 @@ auth.set_access_token("1219820417482993664-My5rqVu32ny9NZmqYCJcVSi0L63Bp0",
                       "Nr8IcbYhLCIS4YEcA9iFo2ZmdS57MpAcNgyDVWhIV2gxh")
 
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
-
+since_id = 1
+new_since_id = since_id
 while 1:
-    search = '@nowtempo'
-    numero = 1
 
-    for tweet in tweepy.Cursor(api.search, search).items(numero):
+    for tweet in tweepy.Cursor(api.mentions_timeline, since_id=since_id).items():
         try:
+                new_since_id = max(tweet.id, new_since_id)
+                if tweet.in_reply_to_status_id is not None:
+                 continue
                 cidade = tweet.text[10:]
                 b = '@!?/0123456789'
                 for i in range(0,len(b)):
-                   cidade = cidade.replace(b[i],"")
-                print(cidade)
+                        cidade = cidade.replace(b[i],"")
+                        print(cidade)
                 requisicao = requests.get('https://api.openweathermap.org/data/2.5/weather?q='+cidade+'&appid=a5f326d36262141e1f7d230ad26db375&lang=pt_br&units=metric')
                 tempo = json.loads(requisicao.text)
                 tempo = tempo['weather'][0]['description']
@@ -30,5 +32,6 @@ while 1:
                 time.sleep(60)
         except tweepy.TweepError as e:
                 print(e.reason)
+                time.sleep(180)
         except StopIteration:
                 break
